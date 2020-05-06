1e7cb76aeb8ae0d02c5b6148a5eadedd01eb3ce9
Louis Gollut <louis.gollut@gmail.com>
2020-05-06 21:22:57 +0200

    Use webpack to bundle js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello world');
+const { imageHelpers } = window.canva;
+const canva = window.canva.init();
+
+canva.onReady(async (opts) => {
+  const img = await imageHelpers.toImageElement(opts.image);
+  document.body.appendChild(img);
+});
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'script.js',
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
